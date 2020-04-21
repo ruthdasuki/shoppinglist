9153ce1a494e40d6a2d768774c3a27ad19c9b71d
Ruth Fan <ruthdasuki@gmail.com>
2020-04-21 15:02:39 -0500

    Set up basics for html and ts files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a text', 'https://unsplash.com/photos/6MT4_Ut8a3Y'),
+    new Recipe('A test recipe', 'This is a text', 'https://unsplash.com/photos/6MT4_Ut8a3Y')
+
+  ];
 
   constructor() { }
 
